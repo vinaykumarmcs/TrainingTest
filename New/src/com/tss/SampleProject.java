32b0c23bae92db7e910c2d4ae30c0d471191e733
vinay kumar <vinaykumar.m@thresholdsoft.com>
2022-11-15 09:32:52 +0530

    Small changes

--- New/src/com/tss/SampleProject.java
+++ New/src/com/tss/SampleProject.java
@@ -3,12 +3,12 @@ package com.tss;
 public class SampleProject {
 
 	public static String PrimeNumber(int number) {
-		 int num = 0, flag = 0; String result=null;
-		num = number / 2;
+		 int value = 0, flag = 0; String result=null;
+		value = number / 2;
 		if (number == 0 || number == 1) {
 			return number + " is not prime number";
 		} else {
-			for (int i = 2; i <= num; i++) {
+			for (int i = 2; i <= value; i++) {
 				if (number % i == 0) {
 					 result=number + " is not prime number";
 					flag = 1;
